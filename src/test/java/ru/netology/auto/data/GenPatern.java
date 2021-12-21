4b20c820672140e13a45223edb74d94cc698a74e
Vladimir Petrochenko <83920626+PerochenkoVA@users.noreply.github.com>
2021-12-21 11:36:45 +0300

    Update GenPatern.java

--- src/test/java/ru/netology/auto/data/GenPatern.java
+++ src/test/java/ru/netology/auto/data/GenPatern.java
@@ -1,30 +1,42 @@
 package ru.netology.auto.data;
 
-import com.github.javafaker.CreditCardType;
 import com.github.javafaker.Faker;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
 
 public class GenPatern {
     private GenPatern() {
     }
 
-    public static class Registeration {
-        private Registeration() {
+    public static class Registration {
+        private Registration() {
         }
 
-        public static RegistrationByCardInfo generate(String local) {
-            Faker faker = new Faker(new Locale(local));
+        public static RegistrationByCardInfo generate(String locale) {
+            Faker faker = new Faker(new Locale("ru"));
             return new RegistrationByCardInfo(
-                    faker.name().fullName(),
-                    faker.finance().creditCard(CreditCardType.VISA),
-                    LocalDate.now().plusYears(1),
-                    faker.address().city()
-            );
-
-
+                    faker.address().city(),
+                    faker.name().lastName() + " " + faker.name().firstName(),
+                    faker.phoneNumber().phoneNumber());
         }
 
+        public static String generateDate(int days){
+            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        }
+        public static String citiesValid(){
+            Random random = new Random();
+            int rand = random.nextInt(7);
+            String cities[] = {"Москва","Санкт-Петербург","Омск","Красноярск","Новосибирск","Казань","Уфа",};
+            return cities[rand];
+        }
+        public static String citiesNotValid(){
+            Random random = new Random();
+            int rand = random.nextInt(7);
+            String citiesNotVal[] = {"Сочи","Енисейск","Выборг","Приморск","Канск","Геленджик","Пушкин"};
+            return citiesNotVal[rand];
+        }
     }
 }
